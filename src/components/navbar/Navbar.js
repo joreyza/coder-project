9ee281b8abf039e20f5d07db8c9bdf6e9a02b2ac
Jorge Reyes <joreyza@gmail.com>
2021-10-09 19:19:04 -0300

    entrega clase 3

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,7 +6,7 @@ export const Navbar = () => {
 
         <div className = "navBar" >
             <ul className='lista'>
-                <li><a href="index.html" ><img src={avatar} alt="Logo" className="foto" /></a></li>
+                <li><a href="index.html" ><img src={avatar} alt="Logo" className="foto" />Sushi JR</a></li>
                 <li> <a href = "about.html" > Sobre nosotros </a></li>
                 <li> <a href = "products.html" > Productos </a></li>
                 <li> <a href = "account.html" > Mi cuenta </a></li>
